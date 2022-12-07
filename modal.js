31f7661ec9e5d962ce2ba6d06158c219e3f46a0b
Hugo Chtlz <93914147+hchtlz@users.noreply.github.com>
2022-12-07 15:05:31 +0100

    ADD: required fields to form and UPD html structure

--- modal.js
+++ modal.js
@@ -13,6 +13,7 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector(".close");
 
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -28,3 +29,26 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+
+// lastname and firstname input empty when submit button is clicked
+function validate() {
+  var x = document.forms["reserve"]["first"].value;
+  if (x == "") {
+    alert("Le prénom doit contenir au moins 2 caractères");
+    return false;
+  }
+
+  var y = document.forms["reserve"]["last"].value;
+  if (y == "") {
+    alert("Le nom doit contenir au moins 2 caractères");
+    return false;
+  }
+
+// checkbox terms required 
+
+  var m = document.getElementById("checkbox1");
+  if (m.checked == false) {
+    alert("Vous devez accepter les termes et conditions");
+    return false;
+  }
+};
